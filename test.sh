1db0fbd6ccf9acc274ebae7066de77ff66512684
Sowrya Gali <galisowrya@gmail.com>
2020-10-27 17:13:28 +0530

    Added comments to bash script to check workflow

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Creates necessary files.
 make
+
+# Iterates through test cases to check lexer and parser.
 for FILE in tests/*; do
 	./parser < $FILE;
 done
+
+# Removes all the created files
 make clean
